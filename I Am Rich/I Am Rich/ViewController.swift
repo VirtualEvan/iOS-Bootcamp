29427af33e5a3f89f24cb6cc00f79fc9175e7d51
Esteban Puentes <virtualevan@gmail.com>
2019-09-25 15:03:56 +0200

    Add "I Am Rich" app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Esteban Puentes on 19.09.19.
+//  Copyright © 2019 Esteban Puentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
