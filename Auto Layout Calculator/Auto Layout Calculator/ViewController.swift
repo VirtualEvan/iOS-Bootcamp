bac0924afe05e9d560b22ca53374dd7141938580
Esteban Puentes <virtualevan@gmail.com>
2019-10-01 14:00:53 +0200

    Add "Auto Layout Calculator" app

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Esteban Puentes on 01.10.19.
+//  Copyright © 2019 Esteban Puentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
